ace6fe9dd7e9676c0a1542f1a3542fbd0baff3f0
Mughal67 <mughalking766766@gmail.com>
2024-08-29 11:47:18 +0500

    "Updated ifss.js to include an if statement and changed script source in index.html to ifss.js"

--- logical.js
+++ logical.js
@@ -3,3 +3,8 @@ var pwd = prompt("enter password", "password");
 
 var message = (uid == "ali" && pwd == "1234") ? "welcome" : "invalid user";
 alert(message);
+ 
+// Here are some more examples of logical operations in JavaScript:
+
+// OR (||): var isAdmin = (uid == "admin" || pwd == "admin"); checks if either the user ID or password is "admin".
+// NOT (!): var isValid = !(uid == "" || pwd == ""); checks if both user ID and password are not empty
