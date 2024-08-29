2faccf7c4321b9fd2e8aced9b5e3cd21e8955810
Mughal67 <mughalking766766@gmail.com>
2024-08-29 13:20:48 +0500

    adding ternary

--- ifss.js
+++ ifss.js
@@ -4,3 +4,36 @@ if(x>10){
 } else{
     document.write(`x is smaller`)
 }
+
+/*
+// if else if 
+let per = 55;
+if (per>=80 && per<=100){
+    document.write(`you are in merit `)
+}else if (per>=60 && per<80);{
+    document.write(`you are in 1 divsion `)
+}else if (per>=45 && per<60);{
+    document.write(`you are in 2 divsion `)
+}else if (per>=33 && per<45);{
+    document.write(`you are 3 divison  `)
+}else if(per<33);{
+    document.write(`you are fail `)
+}*/
+let per = 55;
+
+if (per >= 80 && per <= 100) {
+    document.write(`you are in merit `);
+} else if (per >= 60 && per < 80) {
+    document.write(`you are in 1 divsion `);
+} else if (per >= 45 && per < 60) {
+    document.write(`you are in 2 divsion `);
+} else if (per >= 33 && per < 45) {
+    document.write(`you are 3 divison  `);
+} else if (per < 33) {
+    document.write(`you are fail `);
+}
+//---------ternary opeator-----------------------
+let name = 34;
+let z;
+(name>50) ? z="ture":z="false";
+document.write(z);
---
+++ ternary.js
@@ -0,0 +1,5 @@
+// simple coditional assigment
+let isAdmin = true;
+let message = isAdmin ? "You are an admin" : "You are not an admin";
+document.write(message); // Output: "You are an admin"
+// 
