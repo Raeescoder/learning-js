322bc7c1c17260c7f39fa30fd0461f4ab6675ba2
Mughal67 <mughalking766766@gmail.com>
2024-08-30 22:52:24 +0500

    adding switch case

---
+++ switch.js
@@ -0,0 +1,31 @@
+var day=6;
+console.log(day);
+switch (day)
+{
+    case 0:
+     console.log(`today is monday`)
+    break
+
+    case 1:
+        console.log(`today is tuesday`)
+    break
+    case 2:
+        console.log(`today is wednesday`)
+    break
+    case 3:
+        console.log(`today is thursday`)
+    break
+    case 4:
+        console.log(`today is friday`)   
+    brea
+    case 5:
+        console.log(`today is saturday`)
+    break
+    case 6:
+        console.log(`today is sunday`)
+    break
+
+     default:
+        console.log(`invalid day`)
+     
+}
