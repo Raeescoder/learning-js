516644537f954ecd589841516dd50580c1421124
Mughal67 <mughalking766766@gmail.com>
2024-09-01 12:52:11 +0500

    adding loops

---
+++ loop.js
@@ -0,0 +1,17 @@
+
+// ==============while loop===========================
+// var a =1;
+
+// document.write(`<ul>`)
+// while(a <= 10){
+//     document.write("<li>Item "+a +"</li>");
+//     a=a+1;
+// }
+// document.write(`</ul>`)
+
+//========================do while===============================
+var b=1
+do{
+    document.write(`this is the output <br>`)
+b++;
+}while(b<=20);
