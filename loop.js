d4f3776b9c35e861d75a9d41a7db5106ed0081b9
Mughal67 <mughalking766766@gmail.com>
2024-09-02 13:50:11 +0500

    adding nested file

--- loop.js
+++ loop.js
@@ -1,17 +1,43 @@
 
 // ==============while loop===========================
-// var a =1;
+ var a =1;
 
-// document.write(`<ul>`)
-// while(a <= 10){
-//     document.write("<li>Item "+a +"</li>");
-//     a=a+1;
-// }
-// document.write(`</ul>`)
+ document.write(`<ul>`)
+ while(a <= 10){
+    if(a==9){
+        document.write(`Enough for today`)
+       break 
+    }
+     document.write("<li>Item "+a +"</li>");
+     a=a+1;
+ }
+document.write(`</ul>`)
 
 //========================do while===============================
 var b=1
 do{
     document.write(`this is the output <br>`)
 b++;
-}while(b<=20);
+ }while(b<=20);
+// ================for loop============================
+for( var i=1 ;i<=10; i++){
+    if(i==6){
+        console.log(`hey the nymber is` + i);
+      continue;  
+    }
+    console.log(`The output is` +  i);
+}
+// ===============small task----------
+// 1. write a program to print the  even numbers from 1 to 100
+// 2. write a program to print the odd numbers from 1 to 100
+
+
+for(num=1; num<=100; num++) 
+    {
+    // document.write(num, "<br>")
+    // if(num%2==0) For even 
+    if(num%2 != 0){ //for odd
+
+        document.write(num, "<br>")
+}
+    }
---
+++ nested.js
@@ -0,0 +1,26 @@
+for(var a=1; a<=100;a=a+10){
+for(var b=a;b<a+10; b++){
+    // document.write(b +``)
+}
+document.write(`<br>`)
+}
+// ---------example of nested-----------------
+ for(var c=1; c<=5; c++){
+    for(var d=1; d<=c; d++){
+        // document.write(c +``)
+    }
+    document.write(`<br>`)
+ }
+for(var i=5; i>=1;i--){
+    for(var j=1; j<=i; j++){
+        document.write(i +``)
+}
+document.write(`<br>`)
+}
+
+for(var g=5; g>=1; g--){
+    for(var h=g; h>=1; h--){
+        document.write(h  + ``)
+    }
+    document.write(`<br>`)
+} 
