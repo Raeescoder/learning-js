ba762c4549b9aec9f23df10cff2df8d923256540
Mughal67 <mughalking766766@gmail.com>
2024-09-07 12:50:56 +0500

    modify the file

--- object.js
+++ object.js
@@ -24,3 +24,52 @@ console.log(a.FavMoives[2])
 console.log(a.salary())
 console.log(a.Fullname())
 console.log(a.address)
+
+
+// ===================second method to create object==================================
+ var b = new Object();
+ b.firstName = 'raees ahmad';
+ b.lastName = 'mughal';
+ b.age = 22;
+ b.FavMoives = ["dhoom", "don", "awarapaan"];
+console.log(b);
+// document.write(b ['FavMoives']) another to print the value of object:properties
+
+// --------------------array of object of student------------------------------
+
+var students = [
+  {name: 'raees ahmad', age: 22, gender: 'male'},
+  {name: 'mughal', age: 32, gender: 'male'},
+  {name: 'ali', age: 22, gender: 'male'},
+  {name: 'mohsin', age: 32, gender: 'male'}]
+  console.log(students)
+
+for(var a=0; a<students.length; a++)
+{
+  // document.write(students[a].name + " " + students[a].age + "<br>")
+}
+// --------------constant with object---------------------
+const c = {name: 'raees ahmad', age: 22}
+
+console.log(c)
+  c.name="king khan";
+  c.age=45
+  console.log(c)
+  // c={  name: 'raees ali' }  due of contant 
+  
+// -----------------for in loop-----------------------------
+
+var obj={
+  name: "saad ali" ,
+  age: 22 ,
+  gender: 'male',
+  address: 'lahore'
+
+};
+
+for (var key in obj) {
+  console.log(key)
+  console.log(key + " " + obj[key]);
+
+
+}
