9422f5453999fc32246393ddbc544a92d54daa83
Mughal67 <mughalking766766@gmail.com>
2024-09-09 18:34:57 +0500

    adding math functions

---
+++ math_fun.js
@@ -0,0 +1,38 @@
+var a=3.87;
+console.log(Math.ceil(a)) // to increae the number 
+// ------------floor----------------
+var a=3.87;
+console.log(Math.floor(a)) // to decrease the number
+// ----------round-----------------------
+var a=3.87;
+console.log(Math.round(a)) // to round the number
+// ---------turnc-------------
+var a=576.87;
+console.log(Math.trunc(a)) // to turn the number into integer
+// ------------max-------------------
+var a=3.87;
+var b=3.77;
+console.log(Math.max(a,b)) // to find the max number
+// ===========min-------------
+var a=3.87;
+var b=1.77;
+console.log(Math.min(a,b)) // to find the min number
+// ----------sqrt=================
+var a=9;
+console.log(Math.sqrt(a)) // to find the square root of the number
+// -------------cbrt-------------------
+var a=125;
+console.log(Math.cbrt(a)) // to find the cube root of the number
+// -------------pow============
+var a=2;
+var b=3;
+console.log(Math.pow(a,b)) // to find the power of the number
+// ---------random--------------------
+// console.log Math.floor((Math.random()*10 + 1));-
+console.log(Math.floor(Math.random() * 10 + 1));
+ // to find the random number
+//  -----------abs-----------------
+var a=23.87;
+console.log(Math.abs(a)) // to find the absolute value of the number
+// ---------pi--------------
+console.log(Math.PI) // to find the value of pi
