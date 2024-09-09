f0a45099acb39808f842145e5018cd65ff1eaab7
Mughal67 <mughalking766766@gmail.com>
2024-09-09 18:35:57 +0500

    adding number functions

---
+++ num_fun1.js
@@ -0,0 +1,28 @@
+// ----------number method---------------------
+var a ="99";
+console.log(Number(a)); // to comvert strings into numbers
+// -----------praselnt--------------------------
+
+var b = "50.11";
+
+console.log(parseInt(b)) //to convert the float into interger
+console.log(parseFloat(b)) // to covert the intrerger into float 
+
+
+//-------------------IsINterger--------------------------------------
+
+var c = 123.45;
+console.log(Number.isInteger(c)) // to check the number is integer or not
+// -------------------isfinite----------------------------------------
+var d = 123;
+console.log(Number.isFinite(d)) // to check the number is finite or not
+
+// -------tofixed-----------------------------
+
+var num=3.53444
+console.log(num.toFixed(2)) // to convert the float into string with two decimal places
+// ===============toprecise----------------
+var num1=3.534443
+console.log(num1.toPrecision(2)) // to convert the float into string with two decimal places
+
+ 
