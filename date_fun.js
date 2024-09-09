83f05ac4603a8fe80f73938b91b327d12912899e
Mughal67 <mughalking766766@gmail.com>
2024-09-09 19:10:28 +0500

    adding file of date functions

---
+++ date_fun.js
@@ -0,0 +1,22 @@
+// -----Date functions-----------------
+var now =new Date();
+now.setDate(29)
+now.setFullYear(2025)
+now.setMonth(4)
+now.setHours(14)
+// document.write(now);
+console.log(now);
+console.log(now)
+console.log(now.toDateString()) // to find the actual date 
+console.log(now.getHours())
+console.log(now.getMinutes())
+console.log(now.getSeconds())
+console.log(now.getMilliseconds())
+console.log(now.getDate())
+console.log(now.getMonth()) //return 0 11
+console.log(now.getDay())
+console.log(now.getFullYear())
+console.log(now.getDate() + '/' + now.getMonth() + '/' +now.getFullYear())
+
+
+
